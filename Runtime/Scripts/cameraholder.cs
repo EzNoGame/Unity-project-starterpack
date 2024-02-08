05dbf3c75fb2a628c53880b0a094721539cb5e30
Ed.Zhang <71359408+EzNoGame@users.noreply.github.com>
2024-02-08 16:52:09 -0500

    update

---
+++ Runtime/Scripts/cameraholder.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraholder : MonoBehaviour
+{
+    private void Update() {
+        // if(transform.position.y < 0)
+        // Debug.Log(transform.position);
+    }
+}
